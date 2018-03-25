964aa17fd5fefb1a36882586e2bea4999784da22
Nathan Files <nathanwfiles@gmail.com>
2018-03-25 18:17:26 -0400

    Adds basic Spotify OAuth integration
    - logs access_token to console when loading the app

--- src/SpotifySync.Web/Controllers/HomeController.cs
+++ src/SpotifySync.Web/Controllers/HomeController.cs
@@ -1,17 +1,57 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
+using AspNet.Security.OAuth.Spotify;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SpotifySync.Web.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IAuthenticationService _authService;
+
+        public HomeController(IAuthenticationService authService)
         {
+            _authService = authService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var token = await _authService.GetTokenAsync(
+                HttpContext,
+                SpotifyAuthenticationDefaults.AuthenticationScheme,
+                "access_token");
+
+            ViewData["access_token"] = token;
+
             return View();
+        }
+
+        [HttpGet("signin")]
+        [AllowAnonymous]
+        public IActionResult Login([FromQuery] string redirectUri)
+        {
+            if (string.IsNullOrWhiteSpace(redirectUri))
+            {
+                redirectUri = "/";
+            }
+
+            return Challenge(
+                new AuthenticationProperties { RedirectUri = redirectUri },
+                SpotifyAuthenticationDefaults.AuthenticationScheme
+            );
+        }
+
+        [HttpGet("signout")]
+        public IActionResult Logout()
+        {
+            return SignOut(
+                new AuthenticationProperties { RedirectUri = "/" },
+                CookieAuthenticationDefaults.AuthenticationScheme
+            );
         }
 
         public IActionResult Error()
--- src/SpotifySync.Web/Startup.cs
+++ src/SpotifySync.Web/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
@@ -22,6 +19,30 @@ namespace SpotifySync.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/signin";
+                    options.LogoutPath = "/signout";
+                })
+                .AddSpotify(config =>
+                {
+                    config.ClientId = Configuration.GetValue<string>("ClientId");
+                    config.ClientSecret = Configuration.GetValue<string>("ClientSecret");
+
+                    config.Scope.Add("user-read-currently-playing");
+                    config.Scope.Add("user-modify-playback-state");
+                    config.Scope.Add("user-read-playback-state");
+
+                    config.SaveTokens = true;
+
+                    config.Validate();
+                });
+
             services.AddMvc();
         }
 
@@ -42,6 +63,8 @@ namespace SpotifySync.Web
             }
 
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
