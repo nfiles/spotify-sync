e78620cc378638daf831fd1e68ba15fe22481916
Nathan Files <nathanwfiles@gmail.com>
2018-04-19 09:52:10 -0400

    remove scaffolded items

--- src/SpotifySync.Web/ClientApp/app/app.shared.module.ts
+++ src/SpotifySync.Web/ClientApp/app/app.shared.module.ts
@@ -7,17 +7,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { SigninSpotifyComponent } from './components/signin-spotify/signin-spotify.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent,
-    ],
+    declarations: [AppComponent, NavMenuComponent, HomeComponent],
     imports: [
         CommonModule,
         HttpModule,
@@ -25,8 +18,6 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' },
         ]),
     ],
